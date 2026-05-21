86b2a884d7c03691876cec7c7a0bb6eece0014ff
Vincent Koc <vincentkoc@ieee.org>
2026-05-22 01:09:04 +0800

    fix(ssh): clean up quickssh wrapper

--- bin/bash-completion/quickssh_completion.bash
+++ bin/bash-completion/quickssh_completion.bash
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 _quickssh() {
-    local cur prev opts
+    local cur opts hosts_file ssh_files
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    source $EXTRASPATH/quickssh_hosts.sh
-    opts=$(echo ${!quickssh_hosts[@]})
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+
+    hosts_file="${EXTRASPATH:-$HOME/Library/Mobile Documents/com~apple~CloudDocs/dotfiles/.extra}/quickssh_hosts.sh"
+    if [[ -r "$hosts_file" ]]; then
+        # shellcheck source=/dev/null
+        source "$hosts_file"
+        opts="${quickssh_hosts[*]}"
+    fi
+
+    ssh_files=("$HOME/.ssh/config" "$HOME/.ssh/config.local")
+    if [[ -d "$HOME/.ssh/config.d" ]]; then
+        ssh_files+=("$HOME"/.ssh/config.d/*)
+    fi
+
+    opts+=" $(awk '
+        tolower($1) == "host" {
+            for (i = 2; i <= NF; i++) {
+                if ($i !~ /[*?]/) print $i
+            }
+        }
+    ' "${ssh_files[@]}" 2>/dev/null)"
+
+    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
     return 0
 }
 complete -F _quickssh quickssh
--- bin/zsh-completion/quickssh_completion.zsh
+++ bin/zsh-completion/quickssh_completion.zsh
@@ -1,9 +1,24 @@
 #!/bin/zsh
 
 _quickssh() {
-    local -a subcmds
-    source $EXTRASPATH/quickssh_hosts.sh
-    subcmds=(${(k)quickssh_hosts})
-    _describe 'command' subcmds
+    local hosts_file
+    local -a hosts ssh_files
+
+    hosts_file="${EXTRASPATH:-$HOME/Library/Mobile Documents/com~apple~CloudDocs/dotfiles/.extra}/quickssh_hosts.sh"
+    if [[ -r "$hosts_file" ]]; then
+        source "$hosts_file"
+        hosts+=("${quickssh_hosts[@]}")
+    fi
+
+    ssh_files=("$HOME/.ssh/config" "$HOME/.ssh/config.local" "$HOME/.ssh/config.d/"*(N))
+    hosts+=("${(@f)$(awk '
+        tolower($1) == "host" {
+            for (i = 2; i <= NF; i++) {
+                if ($i !~ /[*?]/) print $i
+            }
+        }
+    ' "${ssh_files[@]}" 2>/dev/null)}")
+
+    _describe 'host' hosts
 }
 compdef _quickssh quickssh
